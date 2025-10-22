54284d6a2f14d01905b84ac5dce4b1880a4f3328
neilo <97009653+neilcarnac@users.noreply.github.com>
2025-10-22 14:46:02 +0530

    Delete my-react-app directory
    we are using react-z as future project

--- my-react-app/src/pages/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <p>This is your dashboard page.</p>
-    </div>
-  );
-}
